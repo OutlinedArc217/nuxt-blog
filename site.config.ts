d6b9ab828e6d0ea6a63423dbb7aae754bf377b8f
OutlinedArc217 <z3407323905@outlook.com>
2024-11-17 15:55:16 +0800

    first commit

---
+++ site.config.ts
@@ -0,0 +1,86 @@
+export const siteConfig = {
+  author: 'OutlinedArc217',
+  title: 'OutlinedArc217 in 🌎',
+  description: 'My blog site.',
+  lang: 'zh-CN',
+}
+
+export const subNavLinks = [
+  {
+    title: 'Blog',
+    path: '/blog',
+  },
+  {
+    title: 'Life',
+    path: '/life',
+  },
+  {
+    title: 'Record',
+    path: '/record',
+  },
+]
+
+export const navLinks = [
+  {
+    title: 'Blog',
+    path: '/blog',
+    icon: 'i-icon-park-outline-align-text-right-one',
+  },
+  {
+    title: 'Projects',
+    path: '/projects',
+    icon: 'i-icon-park-outline-blocks-and-arrows',
+  },
+  {
+    title: 'Tags',
+    path: '/tags',
+    icon: 'i-icon-park-outline-tag-one',
+  },
+  {
+    title: 'Search',
+    path: '/search',
+    icon: 'i-icon-park-outline-search',
+  },
+  {
+    title: 'About',
+    path: '/',
+    icon: 'i-icon-park-outline-grinning-face-with-open-mouth',
+  },
+
+]
+
+export const socialLinks = [
+  {
+    title: 'Telegram',
+    path: 'https://t.me/java_lang_NoClassDefFoundError',
+    icon: 'i-simple-icons-telegram',
+  },
+  {
+    title: 'Bilibili',
+    path: 'https://space.bilibili.com/3850132',
+    icon: 'i-simple-icons-bilibili',
+  },
+  {
+    title: 'Github',
+    path: 'https://github.com/OutlinedArc217',
+    icon: 'i-icon-park-outline-github',
+  },
+]
+
+export const projectList = [
+  {
+    name: 'Projects',
+    content: [
+      {
+        name: 'Nuxt Blog',
+        desc: 'My blog site, base on Nuxt',
+        path: 'https://github.com/OutlinedArc217/nuxt-blog',
+      },
+      {
+        name: '堆栈',
+        desc: 'Just a Resource Site',
+        path: 'https://t.me/libstack',
+      },
+    ],
+  },
+]
