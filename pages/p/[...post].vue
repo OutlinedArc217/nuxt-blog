53bc7b13c32da13bf7e3c935e70a2712ea10c4f9
OutlinedArc217 <z3407323905@outlook.com>
2024-11-17 20:11:17 +0800

    q

--- pages/p/[...post].vue
+++ pages/p/[...post].vue
@@ -14,7 +14,7 @@ const path = post.join('/')
         <doc-render :article="doc" />
         <doc-toc :toc="doc.body.toc" />
         <Giscus
-          repo="chansee97/nuxt-blog" repo-id="R_kgDOJZM7Qg" category="comments"
+          repo="OutlinedArc217/nuxt-blog" repo-id="R_kgDOJZM7Qg" category="comments"
           category-id="DIC_kwDOJZM7Qs4CW1GZ" mapping="title" term="Welcome to my blog!" reactions-enabled="1"
           emit-metadata="1" input-position="top" theme="light_tritanopia" lang="zh-CN" loading="lazy"
         />
